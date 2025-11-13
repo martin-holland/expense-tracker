a835236fbb59a2debcb7a0e2b2530c588b369409
Martin Holland <martin.holland@free.fi>
2025-11-13 02:06:39 +0200

    camera fixes to resolve picture taking on loading

--- composeApp/src/commonMain/kotlin/com/example/expensetracker/services/CameraService.kt
+++ composeApp/src/commonMain/kotlin/com/example/expensetracker/services/CameraService.kt
@@ -11,6 +11,8 @@ interface CameraService {
     suspend fun takePhoto(): ByteArray?
     fun hasCameraPermission(): Boolean
     suspend fun requestCameraPermission(): Boolean
+    fun isCameraReady(): Boolean
+    suspend fun ensureCameraInitialized(): Boolean
 }
 
 expect fun getCameraService(): CameraService
